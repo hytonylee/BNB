9833d9d7e57380b26b57344c1901ebad28cecd6d
Tony Lee <tony.lee@devradius.com>
2021-06-06 22:53:46 -0700

    add event model

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -5,17 +5,50 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="from">From</label>
-                <input type="text" name="from" class="form-control from-control-sm" placeholder="Start Date" />
+                <input
+                    type="text"
+                    name="from"
+                    class="form-control from-control-sm"
+                    placeholder="Start Date"
+                    v-model="from"
+                    @keyup.enter="check"
+                />
             </div>
             <div class="form-group col-md-6">
                 <label for="to">To</label>
-                <input type="text" name="to" class="form-control from-control-sm" placeholder="End Date" />
+                <input
+                    type="text"
+                    name="to"
+                    class="form-control from-control-sm"
+                    placeholder="End Date"
+                    v-model="to"
+                    @keyup.enter="check"
+                />
             </div>
         </div>
 
-        <button class="btn btn-secondary btn-block">Check</button>
+        <button
+            class="btn btn-secondary btn-block"
+            @click="check"
+        >Check</button>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                from: null,
+                to: null
+            }
+        },
+        methods: {
+            check() {
+                alert("CHECK!!")
+            }
+        }
+    }
+</script>
 
 <style scoped>
     label {
