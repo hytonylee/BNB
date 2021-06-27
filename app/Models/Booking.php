0d6624d7117401d472dfb7b0fdadbf3eb406e5e0
Tony Lee <tony.lee@devradius.com>
2021-06-26 20:05:46 -0700

    add new migration

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
+//laravel.com/docs/master/eloquent#events
 class Booking extends Model
 {
     use HasFactory;
